5ffe64008834d4c1f69b15fa3665363ee1ac9c3a
1Maximuse <geofanny.emmanuel@gmail.com>
2020-02-19 10:35:17 +0700

    ganti nomer 2

--- soal2_dekripsi.sh
+++ soal2_dekripsi.sh
@@ -2,5 +2,6 @@
 
 strip='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
 strip2='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-a=$(echo $1 | grep -oP '.*(?=\.txt)' | tr ${strip:$2:26}${strip2:$2:26} ${strip:0:26}${strip2:0:26})
+amt=$(stat -c %y $1 | grep -oP '(?<=[^ ] ).*(?=:.*:)')
+a=$(echo $1 | grep -oP '.*(?=\.txt)' | tr ${strip:$amt:26}${strip2:$amt:26} ${strip:0:26}${strip2:0:26})
 cp $1 $a".txt"
