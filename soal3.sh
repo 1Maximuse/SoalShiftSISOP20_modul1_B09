26af05e32aaf62e079c64be6e64d28fe4f2bc548
1Maximuse <geofanny.emmanuel@gmail.com>
2020-02-19 09:22:17 +0700

    nomer 3

--- soal3.sh
+++ soal3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-for ((i = 1; i <= 28; i++))
-do
-	wget "https://loremflickr.com/320/240/cat" -O "pdkt_kusuma_$i" -a wget.log
-done
+i=$(ls pdkt_kusuma_* | grep -o '[0-9]*' | sort -rn | head -n 1)
+i=$(($i+1))
+wget -O pdkt_kusuma_$i https://loremflickr.com/320/240/cat -a wget.log
--- soal3_identify.sh
+++ soal3_identify.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-mkdir duplicate
+cat wget.log | grep Location: > location.log
+
 mkdir kenangan
-for ((i = 1; i <= 28; i++))
-do
-	if [ -f "pdkt_kusuma_$i" ]
-	then
-		for ((j = $(($i+1)); j <= 28; j++))
-		do
-			if [ -f "pdkt_kusuma_$j" ]
-			then
-				if cmp -s "pdkt_kusuma_$i" "pdkt_kusuma_$j"
-				then
-					mv "pdkt_kusuma_$j" "./duplicate/duplicate_$j"
-				fi
-			fi
-		done
-	fi
-done
-mv -t "./kenangan" `ls | grep "pdkt_kusuma_"`
-cd kenangan
-for f in pdkt_kusuma_*
-do
-	mv "$f" "kenangan_`echo $f | grep -oP '(?<=pdkt_kusuma_)[0-9]*'`"
-done
-cd ..
-cp "wget.log" "wget.log.bak"
+mkdir duplicate
+awk '{
+	i++
+	print i ";" $2
+}' location.log | awk -F ';' '{
+	cnt[$2]++
+	if (cnt[$2] > 1) {
+		cmd = "mv pdkt_kusuma_" $1 " duplicate/duplicate_" $1
+	} else {
+		cmd = "mv pdkt_kusuma_" $1 " kenangan/kenangan_" $1
+	}
+	system(cmd)
+}'
+ls *.log | awk '{
+	cmd = "cp " $0 " " $0 ".bak"
+	system(cmd)
+}'
