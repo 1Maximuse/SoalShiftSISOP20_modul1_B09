6705e3ada23ee33e85d512e15b43bd0f72d6b9bd
1Maximuse <geofanny.emmanuel@gmail.com>
2020-02-21 00:13:59 +0700

    delete old files

--- soal2_dekripsi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-strip='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-strip2='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-amt=$(stat -c %y $1 | grep -oP '(?<=[^ ] ).*(?=:.*:)')
-a=$(echo $1 | grep -oP '.*(?=\.txt)' | tr ${strip:$amt:26}${strip2:$amt:26} ${strip:0:26}${strip2:0:26})
-cp $1 $a".txt"
--- soal2_enkripsi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-strip='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-strip2='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-a=$(echo $1 | grep -oP '.*(?=\.txt)' | tr ${strip:0:26}${strip2:0:26} ${strip:`date +%-H`:26}${strip2:`date +%-H`:26})
-cp $1 $a".txt"
